3340c15ea8cf8330fcd7c26870afb97e4532802b
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-08-10 17:28:52 -0700

    Gen database code

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import sqlite3
 
+def database_creation():
+    print('Generating database...')
+    with sqlite3.connect('Inven-database.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("create table inventory(SKU integer, DESCRIPTION text, PRICE integer)")
+        data = [
+            (10034, 'Gaming key binds', 10)
+        ]
+        cursor.executemany('INSERT INTO inventory VALUES(?,?,?)', data)
+        connection.commit()
+
+        # Querying database
+        cursor.execute("SELECT * FROM inventory")
+        rows = cursor.fetchall()
+        print(rows)
+        
+        connection.commit()
+
+database_creation()
