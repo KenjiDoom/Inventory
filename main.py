71edfeb4d96c2b821db166b6ebb143489cd338f4
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-08-12 13:00:10 -0700

    database_creation() will check for an existing database instead of running two functions.

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 import sqlite3, os
 
-def check_database_existence():
+def database_creation():
     if os.path.isfile('Inven-database.db'):
-        with sqlite3.connect('Inven-database.db') as connection:
-            print('Displaying database information')
+        with sqlite3.connect("Inven-database.db") as connection:
             cursor = connection.cursor()
-            cursor.execute('SELECT * FROM inventory')
+            cursor.execute("SELECT * FROM inventory")
             rows = cursor.fetchall()
+            print('Pre-existing file found...')
             print(rows)
     else:
-        database_creation()
+        with sqlite3.connect('Inven-database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("create table inventory(SKU integer, DESCRIPTION text, PRICE integer)")
+            data = [
+                (10034, 'Gaming key binds', 10)
+            ]
+            cursor.executemany('INSERT INTO inventory VALUES(?,?,?)', data)
+            connection.commit()
 
-def database_creation():
-    print('Generating database...')
-    with sqlite3.connect('Inven-database.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("create table inventory(SKU integer, DESCRIPTION text, PRICE integer)")
-        data = [
-            (10034, 'Gaming key binds', 10)
-        ]
-        cursor.executemany('INSERT INTO inventory VALUES(?,?,?)', data)
-        connection.commit()
+            cursor.execute("SELECT * FROM inventory")
+            rows = cursor.fetchall()
+            print(rows)
 
-        # Querying database
-        cursor.execute("SELECT * FROM inventory")
-        rows = cursor.fetchall()
-        print(rows)
-        
-        connection.commit()
+            connection.commit()
 
-check_database_existence()
+
+database_creation()
