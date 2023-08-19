b7b20aec21622f30563d896d14870ad6199a916e
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-08-19 14:20:50 -0700

    making code look better lol

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3, os, uuid, qrcode
 
 def database_creation():
+    # Note: We cane make these functions more dynamic by using an argument for the database_name instead of hard typing everything out.
     # Checking for stockroom database file [ This represents the stockroom ]
     if os.path.isfile('stockroom-database.db'):
         with sqlite3.connect("stockroom-database.db") as connection:
@@ -53,31 +54,50 @@ def database_creation():
             connection.commit()
 
 def generate_random_uuid():
-    # This is used for idetenifying, product, totes and stockroom inventory.
+    # This is used for identifying, product, totes and stockroom inventory.
     return uuid.uuid4().hex
 
 def generate_qr_codes_for_database(table_name):
     img = qrcode.make(table_name)
     img.save(table_name.replace('.db', '') + '.png')
 
-def create_new_table(database_name):
+def create_new_item_group(database_file_name, table_name):
+    # This function is used for creating a new item group. AKA: creating a new table within a database.
+    # database representing what area of the store you're inserting this item group into. i.e., stockroom or sales floor.
     try:
-        with sqlite3.connect(database_name) as connection:
+        with sqlite3.connect(database_file_name) as connection:
             cursor = connection.cursor()
-            cursor.execute("create table tote(SKU integer, DESCRIPTION text, PRICE integer)")
+            # Table name is the new database name you want to create
+            cursor.execute(f"create table {table_name}(SKU integer, DESCRIPTION text, PRICE integer)")
             connection.commit()
     except sqlite3.OperationalError:
-        print('tote already exist...')
+        print(f'"{table_name}" already exist...')
 
-def show_scan_results(table_name, database_file_name):
-    with sqlite3.connect(database_file_name) as connection:
-        try:
-            cursor = connection.cursor()
-            all_data = cursor.execute(f"SELECT * from {table_name}")
-            print(all_data.fetchall())
-        except sqlite3.OperationalError:
-            print('Table was not found...')
+def show_scan_results_for_item(table_name=None, database_file_name=None):
+    # Show what's inside a database or get description information about a product
+    if database_file_name != None:
+        with sqlite3.connect(database_file_name) as connection:
+            try:
+                cursor = connection.cursor()
+                all_data = cursor.execute(f"SELECT * from {table_name}")
+                print(all_data.fetchall())
+            except sqlite3.OperationalError:
+                if table_name == None:
+                    print('This file does not contain any databases')
+                elif table_name != None:
+                    print(f'{table_name} was not found...')
+
+    else:   
+        print('No database was sepcifeid')
 
 data = input('enter qr code: ')
 list_1 = data.split(" ")
-show_scan_results(list_1[0], list_1[1])
+print(list_1[0])
+try:
+    show_scan_results_for_item(list_1[0], list_1[1])
+except IndexError:
+    show_scan_results_for_item(database_file_name=list_1[0])
+
+
+#print('Create a new table')
+#create_new_item_group(database_file_name='sales_floor_database.db', table_name='testing')
