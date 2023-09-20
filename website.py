1e3cfdde62a16cf013c0315850415fea614f16dd
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-09-20 16:03:05 -0700

    prints out the username and password entered into the input field

--- website.py
+++ website.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -8,9 +7,17 @@ app = Flask(__name__)
 def website():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':        
+        store_number = request.form['storeNumber']
+        username = request.form['username']
+        password = request.form['password']
+
+        print(store_number, username, password)
+
     return render_template('login.html')
 
+        
 if __name__ == "__main__":
-    app.run(host="192.168.1.40", port=4000)
+    app.run(host="192.168.1.81", port=4000)
