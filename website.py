10e405347fdd3913a1cec83fac172470a119d3e7
Kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-10-04 14:53:22 -0700

    checking for table name code

--- website.py
+++ website.py
@@ -72,19 +72,20 @@ def labelpro():
     # 3. Show avaiable QR tag options
     # Idea: Scan SKU or inventory name for generating all the qr tags within that (tote, isle, inventory) (barcode)
     if request.method == 'POST':
-        if request.form.get('item_button') == 'item':
-            try:
-                ID_code = request.form['item_name']
-                # To-do: Detect if string is longer then 5 characters for a scan
-                data = show_scan_results_for_item(unique_code=ID_code)
-                generate_qr_codes_for_sku(sku=data[0], unique_code=data[3], table_name=data[4], database_file_name=data[5])
-                print('Done')
-            except TypeError:
-                return render_template('label.html', error_message='Nothing was entered...')
-        elif request.form.get('inventory_button') == 'inventory':
-            inventory_name = request.form['item_name']
-            print(inventory_name)
-
+        try:
+            if request.form.get('item_button') == 'item':
+                    ID_code = request.form['item_name']
+                    # To-do: Detect if string is longer then 5 characters for a scan
+                    data = show_scan_results_for_item(unique_code=ID_code)
+                    generate_qr_codes_for_sku(sku=data[0], unique_code=data[3], table_name=data[4], database_file_name=data[5])
+                    print('Done')
+            elif request.form.get('inventory_button') == 'inventory':
+                inventory_name = request.form['item_name']
+                print(inventory_name)
+                table = validate_table_name(database=inventory_name)
+                generate_qr_codes_for_database(database=table[0], table_name=table[1])
+        except TypeError:
+            return render_template('label.html', error_message='Nothing was entered...')
     return render_template('label.html')
 
 if __name__ == "__main__":
