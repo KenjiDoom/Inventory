ff967db56a6477ab523fc89170c43f87d774b8f5
kenjidoom <vitodevcool@protonmail.com>
2023-10-26 15:15:35 -0700

    change capacity to all caps

--- create_db_and_users.py
+++ create_db_and_users.py
@@ -16,12 +16,13 @@ def create_product_data():
     with sqlite3.connect('product_information_database.db') as connection:
         cursor = connection.cursor()
         generate_qr_codes_for_database('product_information_database.db', 'product_info')
-        cursor.execute("create table product_info(SKU integer, DESCRIPTION text, PRICE integer, UNIQUE_CODE integer, Capacity integer, LOCATION string)")
+        cursor.execute("create table product_info(SKU integer, DESCRIPTION text, PRICE integer, UNIQUE_CODE integer, CAPACITY integer, LOCATION string)")
         data = [
             (10034, 'Gaming key binds', 10, 3236630686, 7, 'Asile 3, Section 3, Peg #10'),
             (40456, 'Mousepad', 5, 2761298854, 10, 'Asile 2, Section 1, Peg #2'),
             (68896, 'Headset', 15, 5609943232, 4, 'Asile 1, Section 2, Peg #3'),
         ]
+
         cursor.executemany('INSERT INTO product_info VALUES(?,?,?,?,?,?)', data)
         connection.commit()
         cursor.execute("SELECT * FROM product_info")
